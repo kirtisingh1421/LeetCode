7f81b7c0027c780ddcb95866686cc2ce0fd6016f
KIRTI SINGH <kirti.singhh2005@gmail.com>
2026-07-06 12:18:52 +0530

    Time: 1 ms (96.57%), Space: 42.7 MB (36.54%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,9 +1,20 @@
-//by using bit manipulation.
+//by using bit manipulation.(method 1)
+// class Solution {
+//     public boolean isPowerOfTwo(int n) {
+//         if(n ==0) return false;
+//         if(n < 0) return false;
+//         return ((n & (n-1))==0);
+//     }
+// }
+
+//Method 2 using bit manipulation..
 class Solution {
     public boolean isPowerOfTwo(int n) {
         if(n ==0) return false;
         if(n < 0) return false;
-        return ((n & (n-1))==0);
+        if(n == 1) return true;
+        if(n % 2 == 1) return false;
+        return isPowerOfTwo(n/2);
     }
 }
 
