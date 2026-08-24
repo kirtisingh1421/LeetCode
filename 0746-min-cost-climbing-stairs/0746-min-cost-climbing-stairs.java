31d160e5acca090ca33030cf3c0ffe75dbdebcf0
KIRTI SINGH <kirti.singhh2005@gmail.com>
2026-08-24 10:58:35 +0530

    Time: 0 ms (100%), Space: 44.5 MB (94.82%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,14 +1,36 @@
+//space optimzation
 class Solution {
-    static int[] dp;
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
-        dp = new int[n];
-        Arrays.fill(dp, -1);
-        return Math.min(minCost(0,cost),minCost(1,cost));
-    }
-    public int minCost(int i,int[] cost) {
-        if(i >= cost.length) return 0;
-        if(dp[i] != -1) return dp[i];
-        return dp[i] = cost[i]+ Math.min(minCost(i+1,cost),minCost(i+2,cost));
+        if(n==2) return Math.min(cost[0],cost[1]);
+        int [] dp = new int[3];
+        dp[0] = cost[0]; 
+        dp[1] = cost[1];
+        for(int i=2;i<n;i++){
+            dp[2] = cost[i]+Math.min(dp[0],dp[1]);
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+        }
+        return Math.min(dp[0],dp[1]);
     }
 }
+
+
+
+
+//memoization.
+
+// class Solution {
+//     static int[] dp;
+//     public int minCostClimbingStairs(int[] cost) {
+//         int n = cost.length;
+//         dp = new int[n];
+//         Arrays.fill(dp, -1);
+//         return Math.min(minCost(0,cost),minCost(1,cost));
+//     }
+//     public int minCost(int i,int[] cost) {
+//         if(i >= cost.length) return 0;
+//         if(dp[i] != -1) return dp[i];
+//         return dp[i] = cost[i]+ Math.min(minCost(i+1,cost),minCost(i+2,cost));
+//     }
+// }
