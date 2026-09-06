010f95ff5e67368c02aa0246492e164dbe9832b6
KIRTI SINGH <kirti.singhh2005@gmail.com>
2026-09-06 11:23:26 +0530

    Time: 3 ms (98.17%), Space: 47.2 MB (52.45%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minDistance(String a, String b) {
+        int m = a.length();
+        int n = b.length();
+        int [][] dp = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j] = -1;
+            }
+        }
+        return edit(m-1,n-1,new StringBuilder(a),new StringBuilder(b),dp);
+    }
+    public int edit(int i, int j,StringBuilder a, StringBuilder b,int [][] dp) {
+        if(i<0) return j+1;    //inserting remaining character of b 
+        if(j<0) return i+1;    //removing extra character of b.
+        if(dp[i][j] != -1) return dp[i][j];
+        if(a.charAt(i) == b.charAt(j)) return dp[i][j] = edit(i-1,j-1,a,b,dp);
+        int replace = 1 + edit(i-1,j-1,a,b,dp);
+        int remove = 1 + edit(i-1,j,a,b,dp);
+        int rename = 1 + edit(i,j-1,a,b,dp);
+        return dp[i][j] = Math.min(replace, Math.min(rename,remove));
+    }
+}
