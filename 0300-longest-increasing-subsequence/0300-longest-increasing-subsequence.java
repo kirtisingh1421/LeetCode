ead92af6148cfd86c3df08fa8156b5496ed76c0e
KIRTI SINGH <kirti.singhh2005@gmail.com>
2026-09-07 11:53:49 +0530

    Time: 33 ms (76.3%), Space: 46.2 MB (44.01%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,20 +1,44 @@
+//TABULATION.
+
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[n][n+1];
+        int[] dp = new int [n];
+        int maxlen = 1;
+        Arrays.fill(dp,1);
         for(int i=0;i<n;i++){
-            for(int j=0;j<=n;j++){
-                dp[i][j] = -1;
+            int max = 0;
+            for(int j=0;j<i;j++){
+                if(nums[j] < nums[i]){
+                    max = Math.max(max,dp[j]);
+                }
             }
+            dp[i] += max;
+            maxlen = Math.max(maxlen, dp[i]);
         }
-        return helper(0,-1,nums,dp);
-    }
-    public int helper(int idx, int prev, int[] nums, int [][] dp) {
-        if(idx == nums.length) return 0;
-        if(dp[idx][prev+1] != -1) return dp[idx][prev+1];
-        int skip = helper(idx+1,prev,nums,dp);
-        if(prev != -1 && nums[idx] <= nums[prev]) return dp[idx][prev+1] = skip;
-        int pick = 1 + helper(idx+1,idx,nums,dp);
-        return dp[idx][prev+1] = Math.max(pick,skip);
+        return maxlen;
     }
 }
+
+//MEMOIZATION.
+
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         int n = nums.length;
+//         int[][] dp = new int[n][n+1];
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<=n;j++){
+//                 dp[i][j] = -1;
+//             }
+//         }
+//         return helper(0,-1,nums,dp);
+//     }
+//     public int helper(int idx, int prev, int[] nums, int [][] dp) {
+//         if(idx == nums.length) return 0;
+//         if(dp[idx][prev+1] != -1) return dp[idx][prev+1];
+//         int skip = helper(idx+1,prev,nums,dp);
+//         if(prev != -1 && nums[idx] <= nums[prev]) return dp[idx][prev+1] = skip;
+//         int pick = 1 + helper(idx+1,idx,nums,dp);
+//         return dp[idx][prev+1] = Math.max(pick,skip);
+//     }
+// }
